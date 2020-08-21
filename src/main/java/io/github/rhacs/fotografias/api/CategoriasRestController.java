b63642bfe12dffe6e8ea9bdd0010081976c04266
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-20 21:20:59 -0400

    added method obtenerUna

--- src/main/java/io/github/rhacs/fotografias/api/CategoriasRestController.java
+++ src/main/java/io/github/rhacs/fotografias/api/CategoriasRestController.java
@@ -1,6 +1,8 @@
 package io.github.rhacs.fotografias.api;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,6 +11,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -73,6 +76,38 @@ public class CategoriasRestController {
 
         // Devolver listado
         return categorias;
+    }
+
+    /**
+     * Muestra la información de una {@link Categoria}
+     * 
+     * @param id      identificador numérico de la {@link Categoria}
+     * @param request objeto {@link HttpServletRequest} que contiene la información
+     *                de la solicitud que le envía el cliente al servlet
+     * @return un objeto {@link Categoria} con la respuesta a la solicitud
+     */
+    @GetMapping(path = "/{id:^[0-9]+$}")
+    public Categoria obtenerUna(@PathVariable Long id, HttpServletRequest request) {
+        // Depuración
+        depurarSolicitud(request);
+
+        // Buscar información de la Categoría
+        Optional<Categoria> categoria = repositorio.findById(id);
+
+        // Verificar si existe
+        if (categoria.isPresent()) {
+            // Depuración
+            logger.info("[API] Mostrando información de la Categoría: {}", categoria.get());
+
+            // Devolver objeto
+            return categoria.get();
+        }
+
+        // Depuración
+        logger.warn("[API] No se encontró la información para la Categoría con id {}", id);
+
+        // Lanzar excepción
+        throw new NoSuchElementException("No se puede encontrar la Categoría con el identificador numérico " + id);
     }
 
 }
