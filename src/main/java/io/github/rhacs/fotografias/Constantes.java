927b43a5b778c56ca11493394c71278679d1a14e
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-20 20:19:57 -0400

    removed sequences
    added more constants

--- src/main/java/io/github/rhacs/fotografias/Constantes.java
+++ src/main/java/io/github/rhacs/fotografias/Constantes.java
@@ -10,14 +10,35 @@ public class Constantes {
      */
     public static final String TABLA_FOTOGRAFIAS = "fotografias";
 
-    // Secuencias
+    /**
+     * Nombre de la tabla que contiene la información de las {@link Categoria}s
+     */
+    public static final String TABLA_CATEGORIAS = "categorias";
+
+    // Columnas
     // -----------------------------------------------------------------------------------------
 
     /**
-     * Nombre de la secuencia que genera los valores de los identificadores
-     * numéricos para la tabla {@value #TABLA_FOTOGRAFIAS}
+     * Nombre de la llave primaria para la tabla {@value #TABLA_FOTOGRAFIAS}
      */
-    public static final String SECUENCIA_FOTOGRAFIAS = TABLA_FOTOGRAFIAS + "_seq";
+    public static final String FOTOGRAFIAS_ID = "fotografia_id";
+
+    /**
+     * Nombre de la llave primaria para la tabla {@value #TABLA_CATEGORIAS}
+     */
+    public static final String CATEGORIAS_ID = "categoria_id";
+
+    /**
+     * Nombre de la columna que guardará el valor del instante exacto de cuándo se
+     * creó el registro en la tabla
+     */
+    public static final String CREATED_AT = "created_at";
+
+    /**
+     * Nombre de la columna que guardará el valor del instante exacto de cuándo se
+     * actualizó el registro por última vez
+     */
+    public static final String UPDATED_AT = "updated_at";
 
     // Constructores
     // -----------------------------------------------------------------------------------------
