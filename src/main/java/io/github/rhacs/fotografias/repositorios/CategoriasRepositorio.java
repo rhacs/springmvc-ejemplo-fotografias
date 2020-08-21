a8d498efb051e978b53d4bc73d85e20eacf1474d
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-20 22:08:39 -0400

    added method findByNombre

--- src/main/java/io/github/rhacs/fotografias/repositorios/CategoriasRepositorio.java
+++ src/main/java/io/github/rhacs/fotografias/repositorios/CategoriasRepositorio.java
@@ -1,5 +1,7 @@
 package io.github.rhacs.fotografias.repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,13 @@ import io.github.rhacs.fotografias.modelos.Categoria;
 
 @Repository
 public interface CategoriasRepositorio extends JpaRepository<Categoria, Long> {
+
+    /**
+     * Busca una {@link Categoria} en el repositorio a partir del nombre
+     * 
+     * @param nombre nombre a buscar
+     * @return un objeto {@link Optional} con el posible resultado
+     */
+    public Optional<Categoria> findByNombre(String nombre);
 
 }
