77be967d1dc8ee3c7ee3c0152d5b9b4b25efe52f
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-22 15:28:56 -0400

    added initial data for categorias

---
+++ data.sql
@@ -0,0 +1,9 @@
+----------------------------------------------------------------------------------------------------
+-- Tabla: categorias
+----------------------------------------------------------------------------------------------------
+
+INSERT INTO categorias VALUES ("Viajes");
+INSERT INTO categorias VALUES ("Familia");
+INSERT INTO categorias VALUES ("Paisajes");
+INSERT INTO categorias VALUES ("Conciertos");
+INSERT INTO categorias VALUES ("Playa 2019");
