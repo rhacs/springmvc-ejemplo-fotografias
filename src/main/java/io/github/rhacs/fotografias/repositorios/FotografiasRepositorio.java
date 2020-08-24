89ab881f0b03bedac517c8d86bd166549b68d3bc
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-24 16:44:05 -0400

    added method findByUrl

--- src/main/java/io/github/rhacs/fotografias/repositorios/FotografiasRepositorio.java
+++ src/main/java/io/github/rhacs/fotografias/repositorios/FotografiasRepositorio.java
@@ -1,6 +1,7 @@
 package io.github.rhacs.fotografias.repositorios;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -30,5 +31,13 @@ public interface FotografiasRepositorio extends JpaRepository<Fotografia, Long> 
      * @return un objeto {@link List} con el resultado
      */
     public List<Fotografia> findByCategoria(Categoria categoria, Sort sort);
+
+    /**
+     * Busca una {@link Fotografia} en el repositorio basada en la url proporcionada
+     * 
+     * @param url url de la {@link Fotografia}
+     * @return un objeto {@link Optional} que puede o no contener un resultado
+     */
+    public Optional<Fotografia> findByUrl(String url);
 
 }
